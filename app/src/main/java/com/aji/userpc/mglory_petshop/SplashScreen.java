6eed17dcb1850bf7b0a0d2b92b5d65371e909c0f
ajirahmat <ajirahmatmuhajir11@gmail.com>
2018-02-19 10:04:23 +0700

    logo dan icon

---
+++ app/src/main/java/com/aji/userpc/mglory_petshop/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.aji.userpc.mglory_petshop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run () {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
+    }
+
+}
