a40bcf198cc6ecfa0ed817ec170387ffb236113c
Adi Purnomo <adipurnomosidik97@gmail.com>
2018-02-22 15:06:30 +0700

    Login

---
+++ app/src/main/java/com/aji/userpc/mglory_petshop/Login.java
@@ -0,0 +1,23 @@
+package com.aji.userpc.mglory_petshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+
+public class Login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
