c87bd8d089e0c9368c0918dc7f3ce22d314316b8
ajirahmat <ajirahmatmuhajir11@gmail.com>
2018-02-22 18:10:45 +0700

    Revisi yang tadi salah, silahkan download untuk dilanjutkan

--- app/src/main/java/com/aji/userpc/mglory_petshop/MainActivity.java
+++ app/src/main/java/com/aji/userpc/mglory_petshop/MainActivity.java
@@ -1,13 +1,31 @@
 package com.aji.userpc.mglory_petshop;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout ;
+    private ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.main);
+        toggle =  new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
